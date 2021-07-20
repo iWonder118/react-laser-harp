e2d6bb1305ea0dace962c1ac846c6f23b349c26a
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-07-21 00:00:42 +0900

    inital commit

---
+++ src/laserHarp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Guitar from 'react-guitar';
+
+const LaserHarp = () => {
+    return (
+        <div>
+            <Guitar strings={[0, 1, 2, 2, 0 ,-1]} />
+        </div>
+    )
+}
+
+export default LaserHarp;
